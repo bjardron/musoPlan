abcac7620d55c2ff91f43a580839480205714ec6
bjardron <bjardron@gmail.com>
2023-11-17 17:38:53 +1100

    add create troupe function, add comment to functions

--- functions.js
+++ functions.js
@@ -56,3 +56,51 @@ function registerMusician() {
       break;
   }
 }
+
+//this function creates a troupe
+function createTroupe() {
+  const border = '\x1b[36m================================\x1b[0m';
+  console.log(border);
+  console.log('\x1b[36m| Choose a genre for the troupe |\x1b[0m');
+  console.log(border);
+  console.log('\x1b[32m| 1. Rock                       |\x1b[0m');
+  console.log('\x1b[33m| 2. Jazz                       |\x1b[0m');
+  console.log('\x1b[34m| 3. Pop                        |\x1b[0m');
+  console.log(border);
+
+let genreChoice = parseInt(prompt('Enter your genre choice (1, 2, or 3): '));
+
+while (isNaN(genreChoice) || genreChoice < 1 || genreChoice > 3) {
+  console.log('Invalid choice. Please select 1, 2, or 3.'); //error handling for incorrect input by user
+  genreChoice = parseInt(prompt('Enter your genre choice (1, 2, or 3): ')); //prompt uses parse integer to select a number for genre
+}
+
+let genre;
+switch (genreChoice) {
+  case 1:
+    genre = 'Rock';
+    break;
+  case 2:
+    genre = 'Jazz';  //case statement to select a genre
+    break;
+  case 3:
+    genre = 'Pop';
+    break;
+  default:
+    break;
+}
+
+const name = prompt('Enter troupe name: ');
+let duration = parseFloat(prompt('Enter duration (in hours, between 0.5 and 3): '));
+
+if (isNaN(duration) || duration < 0.5 || duration > 3) {  
+  console.log('Duration should be a number between 0.5 and 3 hours.'); //error handling for duration of show. if it is not a number or outside the declared range it will throw an error to user
+  createTroupe();
+  return;
+}
+
+const troupe = new Troupe(name, genre, duration);
+troupes.push(troupe);
+console.log('Troupe created successfully.');
+returnToMenu(true); //boolean 
+}
